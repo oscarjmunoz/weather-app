b15d8260a067d58c106598084909e5bf1243c9a8
oscar <oscarmunoz03@gmail.com>
2023-05-25 22:03:15 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,118 @@
-/*let weather = {
-    "apikey":"c0cd363c192302d3f04f48b125904d49"
-}*/
 
-navigator.geolocation
-geolocation{
-    navigator.geolocation.getCurrentPosition()
-}
+
+let weather = {
+    apiKey: "c0cd363c192302d3f04f48b125904d49",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("Clima no encontrado.");
+            throw new Error("Clima no encontrado.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".descripcion").innerText = descripcion;
+      document.querySelector(".temperatura").innerText = temperatura + "°C";
+      document.querySelector(".humedad").innerText =
+        "humedad: " + humedad + "%";
+      document.querySelector(".viento").innerText =
+        "Velocidad del viento: " + Velocidad + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".barra-de-busqueda").value);
+    },
+  };
+  
+  let geocode = {
+    reverseGeocode: function (latitude, longitude) {
+      var apikey = "2cad6041208649f7bacaf9013ad4ec2e";
+  
+      var api_url = "https://api.opencagedata.com/geocode/v1/json";
+  
+      var request_url =
+        api_url +
+        "?" +
+        "key=" +
+        apikey +
+        "&q=" +
+        encodeURIComponent(latitude + "," + longitude) +
+        "&pretty=1" +
+        "&no_annotations=1";
+  
+      // see full list of required and optional parameters:
+      // https://opencagedata.com/api#forward
+  
+      var request = new XMLHttpRequest();
+      request.open("GET", request_url, true);
+  
+      request.onload = function () {
+        // see full list of possible response codes:
+        // https://opencagedata.com/api#codes
+  
+        if (request.status == 200) {
+          // Success!
+          var data = JSON.parse(request.responseText);
+          weather.fetchWeather(data.results[0].components.city);
+          console.log(data.results[0].components.city)
+        } else if (request.status <= 500) {
+          // We reached our target server, but it returned an error
+  
+          console.log("unable to geocode! Response code: " + request.status);
+          var data = JSON.parse(request.responseText);
+          console.log("error msg: " + data.status.message);
+        } else {
+          console.log("error de servidor");
+        }
+      };
+  
+      request.onerror = function () {
+        // There was a connection error of some sort
+        console.log("unable to connect to server");
+      };
+  
+      request.send(); // make the request
+    },
+    getLocation: function() {
+      function success (data) {
+        geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+      }
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, console.error);
+      }
+      else {
+        weather.fetchWeather("Santa Marta");
+      }
+    }
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  geocode.getLocation();
